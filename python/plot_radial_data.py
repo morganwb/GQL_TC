0cb18fa14474d267d87875d8abbf8bb49c28ddb6
Morgan Baxter <mbaxter@bates.edu>
2019-12-03 12:46:57 -0500

    Changed radial plot script to match Marcus 84 paper 2

--- python/plot_radial_data.py
+++ python/plot_radial_data.py
@@ -14,6 +14,7 @@ import h5py
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (2.6,3.5)
 
 # Parses filename passed to script
 parser = argparse.ArgumentParser(description='Passes filename')
@@ -21,14 +22,32 @@ parser.add_argument('filename', metavar='Rc', type=str, help='.h5 file to plot s
 args = parser.parse_args()
 filename = vars(args)['filename']
 
+eta = 0.875
+mu = 0.
+
+
 # Import .h5 file passed to script 
 datafile = h5py.File(filename,'r')
 
+angular_mom_theta_avg = datafile['tasks/Angular Momentum'][-1,0,:,:].mean(axis=0)
+
+r = datafile['scales/r/1.0'][:]
+
+
+# defined couette flow
+A = (1/eta - 1.)*(mu-eta**2)/(1-eta**2)
+B = eta*(1-mu)/((1-eta)*(1-eta**2))
+v0 = A*r + B/r
+
 # Create plot
 plt.title('Angular Momentum')
-plot_set = datafile['tasks/Angular Momentum'][:,0,0,0]
-plt.plot(range(len(plot_set)),plot_set)
+plot_set = angular_mom_theta_avg
+plt.plot(r,plot_set+(v0*r))
+plt.plot(r,(v0*r))
+plt.scatter(7.2607308623184235, 4.793209944824717)
+plt.scatter(7.67233758246426, 2.6040122651068316)
 plt.tight_layout()
 plot_file_name = 'radial_profile.png'
+
 plt.savefig(plot_file_name, dpi=300)
 
